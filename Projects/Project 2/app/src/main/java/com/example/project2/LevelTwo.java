db242fe092c7eed39da625adda174066fbf9c9d6
Jacky12898 <32112544+Jacky12898@users.noreply.github.com>
2019-12-05 12:42:20 -0700

    project 2 added multiple activities

---
+++ Projects/Project 2/app/src/main/java/com/example/project2/LevelTwo.java
@@ -0,0 +1,72 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class LevelTwo extends AppCompatActivity {
+
+    private CheckBox vertical;
+    private CheckBox horizontal;
+    private Button rotate;
+    private Button move;
+    private Boolean verticalValue = false;
+    private Boolean horizontalValue = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_two);
+
+        vertical = findViewById(R.id.checkBoxVertical);
+        horizontal = findViewById(R.id.checkBoxHorizontal);
+    }
+
+    public void checkBoxes(View view){
+        Boolean isVertical = vertical.isChecked();
+        Boolean isHorizontal = horizontal.isChecked();
+
+        if(isVertical){
+            verticalValue = true;
+        }
+        else{
+            verticalValue = false;
+        }
+
+        if(isHorizontal){
+            horizontalValue = true;
+        }
+        else{
+            horizontalValue = false;
+        }
+    }
+
+    public void rotate(View view){
+        TextView text = findViewById(R.id.text);
+        text.setRotation(text.getRotation() + 90);
+    }
+
+    public void move(View view){
+        TextView text = findViewById(R.id.text);
+
+        if(verticalValue && horizontalValue){
+            text.setX(text.getX() - 50);
+        }
+
+        else if(verticalValue && !horizontalValue){
+            text.setY(text.getY() + 20);
+        }
+
+        else if(!verticalValue && horizontalValue){
+            text.setX(text.getX() - 20);
+        }
+
+        else{
+            text.setY(text.getY() - 20);
+        }
+    }
+}
--- Projects/Project 2/app/src/main/java/com/example/project2/MainActivity.java
+++ Projects/Project 2/app/src/main/java/com/example/project2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,63 +11,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CheckBox vertical;
-    private CheckBox horizontal;
-    private Button rotate;
-    private Button move;
-    private Boolean verticalValue = false;
-    private Boolean horizontalValue = false;
-
+    private boolean levelOneClear = false;
+    private boolean levelTwoClear = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        vertical = findViewById(R.id.checkBoxVertical);
-        horizontal = findViewById(R.id.checkBoxHorizontal);
     }
 
-    public void checkBoxes(View view){
-        Boolean isVertical = vertical.isChecked();
-        Boolean isHorizontal = horizontal.isChecked();
-
-        if(isVertical){
-            verticalValue = true;
-        }
-        else{
-            verticalValue = false;
-        }
-
-        if(isHorizontal){
-            horizontalValue = true;
-        }
-        else{
-            horizontalValue = false;
-        }
+    public void StartLevelOne(View view){
+        Intent intent = new Intent(this, LevelOne.class);
+        startActivity(intent);
     }
 
-    public void rotate(View view){
-        TextView text = findViewById(R.id.text);
-        text.setRotation(text.getRotation() + 90);
-    }
-
-    public void move(View view){
-        TextView text = findViewById(R.id.text);
-
-        if(verticalValue && horizontalValue){
-            text.setX(text.getX() - 50);
-        }
-
-        else if(verticalValue && !horizontalValue){
-            text.setY(text.getY() + 20);
-        }
-
-        else if(!verticalValue && horizontalValue){
-            text.setX(text.getX() - 20);
-        }
-
-        else{
-            text.setY(text.getY() - 20);
-        }
+    public void StartLevelTwo(View view){
+        Intent intent = new Intent(this, LevelTwo.class);
+        startActivity(intent);
     }
 }
